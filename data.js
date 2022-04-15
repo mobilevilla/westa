4acfc092cb9156721409f682fcf082d2ce744a26
Łukasz Fechner <administrator@administrators-MacBook-Pro.local>
2022-04-15 11:59:02 +0200

    init

---
+++ data.js
@@ -0,0 +1,225 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-antresola-1loft-1",
+      "name": "antresola 1(Loft 1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0006776340000005376,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-antresola-1loft-2"
+        },
+        {
+          "yaw": 0.5673365435043713,
+          "pitch": 0.17706399345319568,
+          "rotation": 0,
+          "target": "2-kuchnia--kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-antresola-1loft-2",
+      "name": "antresola 1(Loft 2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-antresola-1loft-1"
+        },
+        {
+          "yaw": -0.4209764601503725,
+          "pitch": 0.3693708827765114,
+          "rotation": 0,
+          "target": "3-salon1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kuchnia--kitchen",
+      "name": "Kuchnia  (Kitchen)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.718453079267448,
+          "pitch": 0.006040742833580026,
+          "rotation": 0,
+          "target": "3-salon1-living-room"
+        },
+        {
+          "yaw": 2.2184435853315385,
+          "pitch": 0.08468755448861032,
+          "rotation": 0,
+          "target": "4-toaleta-toilet"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-salon1-living-room",
+      "name": "Salon1 (Living room)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06764732967352671,
+          "pitch": 0.011755069853709443,
+          "rotation": 0,
+          "target": "0-antresola-1loft-1"
+        },
+        {
+          "yaw": 0.5550166573636197,
+          "pitch": 0.08662279215092283,
+          "rotation": 0,
+          "target": "2-kuchnia--kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-toaleta-toilet",
+      "name": "Toaleta (Toilet)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8910133961245483,
+          "pitch": 0.03699713940557103,
+          "rotation": 0,
+          "target": "2-kuchnia--kitchen"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
